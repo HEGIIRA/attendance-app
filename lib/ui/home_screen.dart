b82817447ff9d9b421f4c2d71242641f2d5fcfa9
HEGIIRA <hegiramecca@gmail.com>
2025-01-21 08:47:49 +0700

    configuring home_screen.dart file

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -10,7 +10,7 @@ class HomeScreen extends StatelessWidget {
     return PopScope( //kyk buat make sure, mau back atau engga, tkut ada data yg ga kesimpen
       canPop: false,
       // ignore: deprecated_member_use
-      onPopInvoked: (didPop) {
+      onPopInvoked: (didPop) { //notif yg bakakal muncul klo mau exit
         if (didPop) {
           return;
         }
@@ -18,48 +18,79 @@ class HomeScreen extends StatelessWidget {
       },
       child: Scaffold(
         backgroundColor: Colors.white,
-        body: SafeArea( //biar ga over lap
+        body: SafeArea( //biar ga over lap biasanya buat app bar/bott app bar
           child: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Container(
-                  margin: const EdgeInsets.all(10),
-                  child: Expanded(
-                    child: InkWell( //yg bisa di pencet
-                      highlightColor: Colors.transparent,
-                      splashColor: Colors.transparent,
-                      onTap: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => const AttendanceScreen()));
-                      },
-                      child: const Column(
-                        children: [
-                          Image(
-                            image: AssetImage('/assets/images/ic_attend.png'),
-                            height: 100,
-                            width: 100,
-                          ),
-                          SizedBox(height: 10),
-                          Text(
-                            "Attendance Report",
-                            style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.bold
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
+                _buildMenuItem(  //ini nyimpen paramet nya ga hrus berurutan, yg bikin berurutan tuh klo
+                  context, 
+                  imagePath: '/assets/images/ic_attend.png',
+                  label: 'Attendance Report',
+                  destination: const AttendanceScreen()
+                  
                 ),
-
-                SizedBox(height: 40)
+                const SizedBox(height: 40),
+                _buildMenuItem(  //ini nyimpen paramet nya ga hrus berurutan, yg bikin berurutan tuh klo
+                  context, 
+                  imagePath: '/assets/images/ic_permission.png',
+                  label: 'Permission Report',
+                  destination: const AttendanceScreen()
+                  
+                ),
+                const SizedBox(height: 40),
+                _buildMenuItem(  //ini nyimpen paramet nya ga hrus berurutan, yg bikin berurutan tuh klo
+                  context, 
+                  imagePath: '/assets/images/ic_attendance_history.png',
+                  label: 'Attendance History',
+                  destination: const AttendanceScreen()
+                  
+                ),
+                const SizedBox(height: 40),
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  Widget _buildMenuItem(
+     //ini parameter yg nampung apa aja yg bakal beda
+    BuildContext context, { //contex juga masuknya parameter
+      required String imagePath, //klo ini parameter yang wajib nanti dan bisa diubah (real data) (dalem kurwal)
+      required String label,
+      required Widget destination //tipe data nya widget karna si navigator itu termasuknya class
+    }) {
+    return Container(
+                margin: const EdgeInsets.all(10),
+                child: Expanded(
+                  child: InkWell( //yg bisa di pencet
+                    highlightColor: Colors.transparent,
+                    splashColor: Colors.transparent,
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => destination)); //jadi dipanggil nama parameter nya
+                    },
+                    child: Column(
+                      children: [
+                        Image(
+                          image: AssetImage(imagePath),
+                          height: 100,
+                          width: 100,
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          label,
+                          style: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              );
   }
 
   Future<bool> _onWillPop(BuildContext context) async {
@@ -106,6 +137,10 @@ class HomeScreen extends StatelessWidget {
           )
         ],
       )
-    ));
+    )
+    //ini adalah default value ketika semua kode yang ada di
+    //blok alert dialog tidak tereksekusi karena beberapa hal
+    )??
+    false;
   }
 }
