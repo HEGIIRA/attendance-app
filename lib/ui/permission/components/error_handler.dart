83472413d8aad21f12b4cc2c99c980d65a721f2b
HEGIIRA <hegiramecca@gmail.com>
2025-02-05 11:09:44 +0700

    create error_handler

---
+++ lib/ui/permission/components/error_handler.dart
@@ -0,0 +1,12 @@
+import 'package:attendance_app/ui/permission/components/snack_bar.dart';
+import 'package:flutter/material.dart';
+
+class ErrorHandler {
+  static void handleError(BuildContext context, dynamic error) {
+    SnackBarComponent.showSnackBar(
+      context,
+      error,
+      isError: true,
+    );
+  }
+}
