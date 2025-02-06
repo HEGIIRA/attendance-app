466a143f0f2cfc4aa60f5f1815462d81807719ee
HEGIIRA <hegiramecca@gmail.com>
2025-02-06 10:38:06 +0700

    finishin error_handler.dart

--- lib/ui/permission/components/error_handler.dart
+++ lib/ui/permission/components/error_handler.dart
@@ -1,12 +1,32 @@
 import 'package:attendance_app/ui/permission/components/snack_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
+//perkondisian terait snakbar
 class ErrorHandler {
   static void handleError(BuildContext context, dynamic error) {
-    SnackBarComponent.showSnackBar(
-      context,
-      error,
-      isError: true,
-    );
+    if (error is PermissionDeniedException) {
+      SnackBarComponent.showSnackBar(
+        context, 
+        "Permission Denied",
+        isError: true //ngasih tau kalau ini error permission
+      );
+    } else {
+      SnackBarComponent.showSnackBar(
+        context,
+        error, //ini bakal napilin error sesuai error nya, bukan yg permission,
+        isError: true,
+      );
+    }
+    Navigator.of(context).pop();
   }
+}
+
+class PermissionDeniedException implements Exception {
+  final String message;
+  //proses pembuatan placeholder dri sebuah exception/error
+  PermissionDeniedException({this.message = ''});
+
+  @override
+  String toString() => message.isEmpty ? "Permission Denied" : message; //ini inline/anonymus ife condition
 }
